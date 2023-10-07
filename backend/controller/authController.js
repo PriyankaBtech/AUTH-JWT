3b585bf163439ad1cd10c8915a4a4eb11fede08c
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-10-07 13:01:04 +0530

    add validation for signup in controllers

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,8 +1,31 @@
 const userModel = require("../model/userSchema")
+const emailValidator = require('email-validator')
 
 const signup = async (req, res, next) => {
     const {name, email, password, confirmPassword} = req.body
     console.log(name, email, password, confirmPassword)
+
+    if (!name || !email || !password || !confirmPassword) {
+        return res.status(400).json({
+            success: false,
+            message: "Every field is required"
+        })
+    }
+
+    const validEmail = emailValidator.validate(email)
+    if (!validEmail) {
+        return res.status(400).json({
+            success: false,
+            message: "Please provide valid email id"
+        })            
+    }
+    
+    if (password !== confirmPassword) {
+        return res.status(400).json({
+            success: false,
+            message: "password and confim password doesn't match"
+        })         
+    }
 
     try {
         const userInfo = userModel(req.body)
