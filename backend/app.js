d46d645797b90a1fe5f4a62da6a67c19e35d5905
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-10-07 12:21:58 +0530

    add signup detail detail in DB

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 
 const express = require('express')
 const app = express()
-const authRouter = require('./router/authRoute')
+const authRouter = require('./router/authRoute.js')
+const connectToDB = require('./config/databaseConfig.js')
 
 app.use(express.json())
+connectToDB()
 
 app.use('/api/auth/', authRouter)
 
--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,13 +1,34 @@
+const userModel = require("../model/userSchema")
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
     const {name, email, password, confirmPassword} = req.body
-    console.log(name, email, password, confirmPassword) 
+    console.log(name, email, password, confirmPassword)
 
-    return res.status(200).json({
-        success: true,
-        date: {}
-    })
+    try {
+        const userInfo = userModel(req.body)
+        // directly store in database
+        const result = await userInfo.save()
+    
+        return res.status(200).json({
+            success: true,
+            date: result
+        })
 
+        
+    } catch (error) {
+        if (error.code === 11000) {
+            return res.status(400).json({
+                success: false,
+                message: 'Account already exists with provided email id'
+            })            
+        }
+
+        return res.status(400).json({
+            success: false,
+            message: error.message
+        })
+        
+    }
 }
 
 module.exports = {
