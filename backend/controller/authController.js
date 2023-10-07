e331d3864fa967d11bb572a8290dc6b27d09343f
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-10-07 17:46:55 +0530

    add signin with jwt token

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,6 +1,7 @@
 const userModel = require("../model/userSchema")
 const emailValidator = require('email-validator')
 
+// sign-up
 const signup = async (req, res, next) => {
     const {name, email, password, confirmPassword} = req.body
     console.log(name, email, password, confirmPassword)
@@ -54,6 +55,55 @@ const signup = async (req, res, next) => {
     }
 }
 
+// sign-in
+const signin = async (req, res) => {
+    const {email, password} = req.body
+
+    if (!email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: "Every field is mandatory"
+        })        
+    }
+
+    try {
+        // check the entered email id exists in DB or not
+        const user = await userModel
+            .findOne({
+               email
+        })
+        .select('+password')
+
+        if (!user || user.password !== password) {
+            return res.status(400).json({
+            success: false,
+            message: "Invalid credentials"
+        })         
+    }
+
+    const token = user.jwtToken()
+    user.password = undefined
+
+    const cookieOption = {
+        maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: true    
+    }
+
+    res.cookie("token", token, cookieOption)
+    res.status(200).json({
+        success: true,
+        data: user 
+    })
+        
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: error.message 
+        })        
+    }
+}
+
 module.exports = {
-    signup
+    signup,
+    signin
 }
--- backend/model/userSchema.js
+++ backend/model/userSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
+const JWT = require('jsonwebtoken')
 
 const userSchema = new Schema({
     name: {
@@ -30,6 +31,16 @@ const userSchema = new Schema({
 }, {
     timestamps: true
 })
+
+userSchema.methods = {
+    jwtToken() {
+        return JWT.sign(
+            {id: this._id, email: this.email,},
+            process.env.SECRET,
+            {expiresIn: '24h'}
+        )
+    }
+}
 
 const userModel = mongoose.model("User", userSchema)
 
